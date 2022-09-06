b56683a922e13ef415d3eaf5308f73eb320ca04a
Lord-A2K <amirkiankaveh@gmail.com>
2022-09-06 12:44:40 +0430

    complete comment card foe comment section

--- lib/components/comment_card.dart
+++ lib/components/comment_card.dart
@@ -1,3 +1,4 @@
+import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 import 'package:kluksgram/models/user.dart';
 import 'package:kluksgram/providers/user_provider.dart';
@@ -10,27 +11,57 @@ class CommentCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User? _user = Provider.of<UserProvider>(context).getUser;
-    return _user == null
-        ? Center(child: CircularProgressIndicator())
-        : Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: Row(
-              children: [
-                Container(
-                  height: 60,
-                  width: 60,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(_user!.photoUrl),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
-                    child: Text(snap['text']),
-                  ),
-                ),
-              ],
+    var size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+          color: Colors.black.withOpacity(.4),
+          borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 60,
+            width: 60,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(_user!.photoUrl),
             ),
-          );
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 5),
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                    color: Colors.black26,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  _user.username,
+                  style: TextStyle(
+                    fontSize: size.width * .035,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10),
+                width: size.width * .6,
+                height: size.height * .1,
+                child: SingleChildScrollView(
+                  child: Text(
+                    snap['text'],
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/components/comment_section.dart
+++ lib/components/comment_section.dart
@@ -6,6 +6,7 @@ import 'package:kluksgram/models/user.dart';
 import 'package:kluksgram/providers/user_provider.dart';
 import 'package:kluksgram/resources/firestore_method.dart';
 import 'package:kluksgram/utils/appcolors.dart';
+import 'package:kluksgram/utils/utils.dart';
 import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 
@@ -30,119 +31,117 @@ class _CommentSectionState extends State<CommentSection> {
     User? _user = Provider.of<UserProvider>(context).getUser;
     var size = MediaQuery.of(context).size;
 
-    return _user == null
-        ? Center(child: CircularProgressIndicator())
-        : Container(
-            height: size.height * .7,
-            child: Stack(
-              children: [
-                // bg
-                Blur(
-                  blur: 5,
-                  colorOpacity: .1,
-                  blurColor: Colors.transparent,
-                  child: Container(
-                    height: size.height * .7,
-                    width: size.width,
-                    // color: Colors.black45,
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                      width: 50,
-                      child: Lottie.asset('assets/animations/swipedown.json')),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 10, vertical: 30),
-                    child: Expanded(
-                      child: StreamBuilder(
-                          stream: FirebaseFirestore.instance
-                              .collection('posts')
-                              .doc(widget.snap['postId'])
-                              .collection('comments')
-                              .snapshots(),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            }
-                            if (snapshot.hasData) {
-                              return ListView.builder(
-                                itemCount:
-                                    (snapshot.data! as dynamic).docs.length,
-                                itemBuilder: (context, index) => CommentCard(
-                                  snap: snapshot
-                                      .data!
-                                      .docs[snapshot.data!.docs.length -
-                                          index -
-                                          1]
-                                      .data(),
-                                ),
-                              );
-                            } else {
-                              return Center(
-                                child: Text('No Comments'),
-                              );
-                            }
-                          }),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 15),
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 60,
-                          width: 60,
-                          child: CircleAvatar(
-                            backgroundImage: NetworkImage(_user!.photoUrl),
-                          ),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            controller: _commentController,
-                            decoration: InputDecoration(
-                              hintText: 'Comment as ${_user.username}',
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide.none),
-                            ),
-                            minLines: 1,
-                            maxLines: 4,
-                          ),
-                        ),
-                        InkWell(
-                          onTap: () async {
-                            String res = await FirestoreMethod().postComment(
-                                widget.snap['postId'],
-                                _commentController.text,
-                                _user.uid,
-                                _user.username,
-                                _user.photoUrl);
-                          },
-                          child: Container(
-                            // height: 25,
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                                color: AppColors().accent,
-                                borderRadius: BorderRadius.circular(50)),
-                            child: Text('Post'),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
+    return Container(
+      height: size.height * .7,
+      child: Stack(
+        children: [
+          // bg
+          Blur(
+            blur: 5,
+            colorOpacity: .1,
+            blurColor: Colors.transparent,
+            child: Container(
+              height: size.height * .7,
+              width: size.width,
+              // color: Colors.black45,
             ),
-          );
+          ),
+          Positioned(
+            left: (size.width / 2) - 35,
+            top: -20,
+            child: Container(
+                width: 70,
+                child: Lottie.asset('assets/animations/swipedown.json')),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 10, right: 10, top: 40, bottom: 80),
+              child: Expanded(
+                child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('posts')
+                        .doc(widget.snap['postId'])
+                        .collection('comments')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          itemCount: (snapshot.data! as dynamic).docs.length,
+                          itemBuilder: (context, index) => CommentCard(
+                            snap: snapshot.data!
+                                .docs[snapshot.data!.docs.length - index - 1]
+                                .data(),
+                          ),
+                        );
+                      } else {
+                        return Center(
+                          child: Text('No Comments'),
+                        );
+                      }
+                    }),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Row(
+                children: [
+                  Container(
+                    height: 60,
+                    width: 60,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(_user!.photoUrl),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: _commentController,
+                      decoration: InputDecoration(
+                        hintText: 'Comment as ${_user.username}',
+                        border: OutlineInputBorder(borderSide: BorderSide.none),
+                      ),
+                      minLines: 1,
+                      maxLines: 4,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      String res = await FirestoreMethod().postComment(
+                          widget.snap['postId'],
+                          _commentController.text,
+                          _user.uid,
+                          _user.username,
+                          _user.photoUrl);
+                      if (res == 'success') {
+                        _commentController.text = '';
+                        showSnackbar('comment posted.', context);
+                        FocusManager.instance.primaryFocus?.unfocus();
+                        
+                      }
+                    },
+                    child: Container(
+                      // height: 25,
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: AppColors().accent,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: Text('Post'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
